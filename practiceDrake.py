f099dd432935b6644899739e329657bac04c47d4
Drake Beuter <drakebeuter@Drakes-MacBook-Air.local>
2020-02-27 09:24:14 -0800

    finished code

--- practiceDrake.py
+++ practiceDrake.py
@@ -2,18 +2,22 @@
 #partner 1 name: Drake
 #partner 2 name: Nic
 ###############################################################################
-#This function does things
+#This function generates 10 random numbers and returns the product of those 10 numbers.
+import random as rand
+import numpy as np
 def getNRandom(n):
 	'''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
-    pass
+	l = []
+	for i in range(n):
+		l.append(rand.randint(1,10))
+	return l
 
 def multiplyRandom(numbers):
 	'''takes in a list of n numbers and returns the product of the numbers'''
-    pass
+	return np.prod(numbers)
 
 def main():
-	print(multiplyRandom(getNRandom(10))
+	print(multiplyRandom(getNRandom(10)))
 
 if __name__ == "__main__":
 	main()
-
